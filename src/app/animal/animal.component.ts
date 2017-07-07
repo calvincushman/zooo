a47d9c891e6af149b49adc7933d67b156845fbea
Calvin Cushman <calvincushman@Calvins-MacBook-Pro.local>
2017-07-07 16:15:19 -0700

    move basic list functionality into it's own file, refactor html and create new methods

---
+++ src/app/animal/animal.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { Animal } from '../animal.model';
+
+@Component({
+  selector: 'animal-list',
+  template: `
+  <ul>
+    <li *ngFor="let currentAnimal of childAnimalList">
+      <ul>
+        <img src="{{currentAnimal.picture}}">
+        <li>{{currentAnimal.name}}</li>
+        <li>{{currentAnimal.species}}</li>
+        <li>{{currentAnimal.sex}}</li>
+        <li>{{currentAnimal.age}}</li>
+        <li>{{currentAnimal.location}}</li>
+        <li>{{currentAnimal.diet}}</li>
+      </ul>
+      <button (click)="editButtonClicked(currentAnimal)">Edit Details</button>
+    </li>
+  </ul>
+  `
+})
+export class AnimalListComponent implements OnInit {
+  @Input() childAnimalList: Animal[];
+  @Output() clickSender = new EventEmitter();
+
+  editButtonClicked(animalToEdit: Animal) {
+    this.clickSender.emit(animalToEdit);
+  }
+
+  ngOnInit() {
+  }
+
+}
