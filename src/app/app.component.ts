d98ee68d3e7645aa73c8906fd926c1bd004c70a3
Calvin Cushman <calvincushman@Calvins-MacBook-Pro.local>
2017-07-07 14:42:28 -0700

    start over, create initial file structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div class="container">
+    <h1>World of Animals</h1>
+    <ul>
+      <li>{{firstAnimal.name}}</li>
+    </ul>
+  </div>
+  `
 })
 export class AppComponent {
-  title = 'app works!';
+
 }
