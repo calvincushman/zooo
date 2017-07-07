cd687ac80d5137e448b780a27643756a435c469c
Calvin Cushman <calvincushman@Calvins-MacBook-Pro.local>
2017-07-07 16:13:26 -0700

    move animal model into it's own remote file

---
+++ src/app/animal.model.ts
@@ -0,0 +1,3 @@
+export class Animal {
+  constructor(public picture: string, public name: string, public species: string, public sex: string, public age: number, public location: string, public diet: string) { }
+}
