94336550124da2c4e57c88dc061a1ba197d9023f
Calvin Cushman <calvincushman@Calvins-MacBook-Pro.local>
2017-07-08 12:51:40 -0700

    finish builing loops in class BabyPipe

--- src/app/baby.pipe.ts
+++ src/app/baby.pipe.ts
@@ -3,17 +3,32 @@ import { Animal } from './animal.model';
 
 @Pipe({
   name: "baby",
-  pure: false
+  pure: baby
 })
 
 export class BabyPipe implements PipeTransform {
-  transform(input: Animal[]) {
+  transform(input: Animal[], desiredAgeGroup) {
     var output: Animal[]=[];
+    if(desiredAgeGroup === "babyAnimals") {
     for (var i=0; i < input.length; i++) {
       if (input[i].age <= 2) {
         output.push(input[i]);
       }
     }
     return output;
+  } else if (desiredAgeGroup === "adultAnimals") {
+    for (var i=0; i < input.length; i++) {
+      if (input[i].age >= 2) {
+        output.push(input[i]);
+      }
+    }
+    return output;
+  } else (desiredAgeGroup === "allAnimals") {
+    for (var i=0; i < input.length; i ++) {
+      output.push(input[i]);
+    }
+    return output
+  } else{
+    return input;
   }
 }
